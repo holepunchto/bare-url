6c7cfcb1a3f1a05ff11fdabab78a8914c5d19eb8
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-08-11 09:57:35 +0200

    Improve overall performance (#34)

--- test/search-params.js
+++ test/search-params.js
@@ -14,6 +14,61 @@ test('construct from leading ?', (t) => {
   t.is(params.get('foo'), '1')
 })
 
+test('construct from empty string', (t) => {
+  t.is(new URLSearchParams('').size, 0)
+  t.is(new URLSearchParams('?').size, 0)
+})
+
+test('construct from string, percent decodes', (t) => {
+  const params = new URLSearchParams('foo%20bar=baz%26quux&a%3Db=c%3Dd')
+
+  t.is(params.get('foo bar'), 'baz&quux')
+  t.is(params.get('a=b'), 'c=d')
+})
+
+test('construct from string, percent decodes non-ASCII', (t) => {
+  const params = new URLSearchParams('n%C3%A6vn=v%C3%A6rdi')
+
+  t.is(params.get('n\u00e6vn'), 'v\u00e6rdi')
+})
+
+test('construct from string, decodes + as space', (t) => {
+  const params = new URLSearchParams('foo+bar=baz+quux')
+
+  t.is(params.get('foo bar'), 'baz quux')
+})
+
+test('construct from string, decodes + and percent together', (t) => {
+  const params = new URLSearchParams('a+b%2Bc=d+e%2Bf')
+
+  t.is(params.get('a b+c'), 'd e+f')
+})
+
+test('construct from string, value containing =', (t) => {
+  const params = new URLSearchParams('foo=bar=baz')
+
+  t.is(params.get('foo'), 'bar=baz')
+})
+
+test('construct from string, name without value', (t) => {
+  const params = new URLSearchParams('foo&bar=1')
+
+  t.is(params.get('foo'), '')
+  t.is(params.get('bar'), '1')
+})
+
+test('construct from string, empty value', (t) => {
+  t.is(new URLSearchParams('foo=').get('foo'), '')
+})
+
+test('construct from string, skips empty sequences', (t) => {
+  const params = new URLSearchParams('&foo=1&&bar=2&')
+
+  t.is(params.size, 2)
+  t.is(params.get('foo'), '1')
+  t.is(params.get('bar'), '2')
+})
+
 test('construct from object', (t) => {
   const params = new URLSearchParams({ foo: '1', bar: '2' })
 
@@ -165,6 +220,31 @@ test('toString, encodes the urlencoded byte set', (t) => {
   params.append("a b!'()~*-._", '')
 
   t.is(params.toString(), 'a+b%21%27%28%29%7E*-._=')
+})
+
+test('toString, encodes non-ASCII as UTF-8', (t) => {
+  const params = new URLSearchParams()
+
+  params.append('n\u00e6vn', 'v\u00e6rdi')
+  params.append('emoji', '\u{1f600}')
+
+  t.is(params.toString(), 'n%C3%A6vn=v%C3%A6rdi&emoji=%F0%9F%98%80')
+})
+
+test('toString, replaces lone surrogates', (t) => {
+  const params = new URLSearchParams()
+
+  // A surrogate that is not part of a pair encodes no scalar value, so the UTF-8
+  // encoder substitutes U+FFFD for it.
+  params.append('\ud800', 'a\udfffb')
+
+  t.is(params.toString(), '%EF%BF%BD=a%EF%BF%BDb')
+})
+
+test('toString, round trips through the parser', (t) => {
+  const input = 'a+b=c%26d&n%C3%A6vn=v%C3%A6rdi&plain=1'
+
+  t.is(new URLSearchParams(new URLSearchParams(input).toString()).toString(), input)
 })
 
 test('toJSON', (t) => {
